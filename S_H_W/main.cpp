70d31b4202a0cf195cfdc915629c7e1f27a4e43e
Shadowpatriot9 <gscovil9@gmail.com>
2025-02-01 17:33:43 -0700

    Initial Commit

---
+++ S_H_W/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  S_H_W
+//
+//  Created by Grayden Scovil on 2/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
