bf0fa7f33e927e53619d9d7c709b5667d692d8b5
Shadowpatriot9 <gscovil9@gmail.com>
2025-02-03 15:53:44 -0700

    -commiting this, then Im playing a game - was trying to make a bubble sort for a random string of numbers but couldnt figure it out on my own and didnt want to resort to copying and pasting so just instead made a input text displayer and a loop to show an array of values

--- S_H_W/main.cpp
+++ S_H_W/main.cpp
@@ -2,13 +2,21 @@
 #include <string>
 using namespace std;
 
-char hello(char message) {
-    cout << "Hello World" << "\n";
+char hello(string message) {
+    cout << message << "\n";
     return 0;
 }
 
 int main() {
-    char message {};
+    string message {};
+    cout << "Enter text:";
+    cin >> message;
     hello(message);
+    
+    int values[5] = {3,2,1,4,5};
+    for (int i = 0; i < values[4] ; i++) {
+        cout << values[i];
+    }
+    cout << "\n";
     return 0;
 }
