d764c00abe3484079d4e5d772d6aef818e8822c1
Shadowpatriot9 <gscovil9@gmail.com>
2025-02-04 11:25:28 -0700

    -bubblesort added -remove previous buuble function -sync

--- S_H_W/main.cpp
+++ S_H_W/main.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-char hello(string message) {
+char hello() {
+    string message {};
+    cout << "Enter text:";
+    cin >> message;
     cout << message << "\n";
     return 0;
 }
 
-int main() {
-    string message {};
-    cout << "Enter text:";
-    cin >> message;
-    hello(message);
-    
-    int values[5] = {3,2,1,4,5};
-    for (int i = 0; i < values[4] ; i++) {
-        cout << values[i];
+int bubbleSort() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    vector<int> numbers(n);
+    cout << "Enter" << n << " integers:\n";
+    for (int i = 0; i < n; ++i) {
+        cin >> numbers[i];
     }
+    
+    for (int i = 0; i<n-1 ; i++){
+        for (int j =0; j<n-i-1; ++j){
+            if (numbers[j]>numbers[j+1]){
+                int temp = numbers[j];
+                numbers[j] = numbers[j+1];
+                numbers[j+1] = temp;
+            }
+        }
+    }
+    
+    cout << "Output:\n";
+    for (int i = 0; i<n; ++i){
+        cout << numbers[i] << " ";
+    }
+    return 0;
+}
+
+int main() {
+//    hello();
+//    bubble();
+    bubbleSort();
+    
     cout << "\n";
     return 0;
 }
+
+
